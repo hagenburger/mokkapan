608285ffdd07a56dc54309171a7f637adc522183
Nico Hagenburger <nico@hagenburger.net>
2012-07-28 23:09:01 +0200

    output optimizations

--- config.rb
+++ config.rb
@@ -58,10 +58,10 @@ set :relative_links, true
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
@@ -76,4 +76,6 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+
+  set :haml, format: :html5, ugly: true, attr_wrapper: '"'
 end
